0e078eee2ffb8c644d28aa3d5517b86c9f149bf1
Artur <Student@193.107.34.220>
2018-03-13 11:39:38 +0100

    Lab3.1

--- Cities/src/cities/Countries.java
+++ Cities/src/cities/Countries.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Countries {
 
     private Map<String, String> countries = new HashMap();
-
+    
     public Countries() {
         countries.put("Poland", "Warsaw");
         countries.put("German", "Berlin");
@@ -16,6 +16,66 @@ public class Countries {
     }
 
     @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.countries);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Countries other = (Countries) obj;
+        if (!Objects.equals(this.countries, other.countries)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    public Collection<String> capitals()
+        {
+           return countries.keySet();  
+        }
+         
+        public Collection<String> countries()
+        {
+           return countries.values();  
+        }
+        
+         public Collection<String> sortedCapitals()
+        {
+           TreeSet set = new TreeSet();
+           set.addAll(capitals());
+           return set;
+        }
+         
+        public Collection<String> sortedCountries()
+        {
+          TreeSet set = new TreeSet();
+           set.addAll(countries());
+           return set;
+        }
+        public Map<String,String> sortedByCountries()
+        {
+          TreeMap map = new TreeMap();
+        }
+          public Map<String,String> sortedByCapitals()
+        {
+          
+        }
+       
+    
+    
+    @Override
     public String toString() {
         return countries.toString();
     }
@@ -24,6 +84,11 @@ public class Countries {
         Countries c = new Countries();
         System.out.println(c);
 
+        System.out.println(c.capitals());
+        System.out.println(c.countries());
+  
+
+
     }
-//dasd
+    
 }
